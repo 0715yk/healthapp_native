bd96beb13237cbb83cc9b422c2c23c6d4666a2df
0715yk <yongkiniiii@gmail.com>
2023-05-09 21:52:08 +0900

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import WebView from "react-native-webview";
+import { SafeAreaView, StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <WebView source={{ uri: "https://d1v4qnuyx40ush.cloudfront.net/" }} />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
   },
 });
+export default App;
